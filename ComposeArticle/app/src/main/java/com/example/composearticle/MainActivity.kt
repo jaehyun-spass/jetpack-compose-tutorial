c0143ec41b6979cb047c7a61b24e4d0f1339628a
Jaehyun <recreate@spass.ai>
2022-12-27 12:49:39 +0900

    Done "ComposeArticle" app

--- ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.composearticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +28,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeArticleTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    TutorialScreen()
                 }
             }
         }
@@ -30,14 +37,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TutorialScreen() {
+    Column {
+        val image = painterResource(id = R.drawable.bg_compose_background)
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.FillWidth
+        )
+        Text(
+            text = stringResource(R.string.tutorial_title),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(all = 16.dp)
+        )
+        Text(
+            text = stringResource(R.string.tutorial_description1),
+            modifier = Modifier.padding(horizontal = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = stringResource(R.string.tutorial_description2),
+            modifier = Modifier.padding(all = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeArticleTheme {
-        Greeting("Android")
+        TutorialScreen()
     }
 }
--- ComposeArticle/app/src/main/java/com/example/composearticle/ui/theme/Theme.kt
+++ ComposeArticle/app/src/main/java/com/example/composearticle/ui/theme/Theme.kt
@@ -28,10 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun ComposeArticleTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
+fun ComposeArticleTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
