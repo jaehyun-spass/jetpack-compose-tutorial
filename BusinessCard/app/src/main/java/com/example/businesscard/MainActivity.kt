d711554820b814838362e6939e7115d559c7924f
Jaehyun <recreate@spass.ai>
2022-12-27 18:01:17 +0900

    Add Composable "MainProfile"

--- BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
+++ BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +27,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = Color(0xFF0a3041)
                 ) {
-                    Greeting("Android")
+                    AppScreen()
                 }
             }
         }
@@ -30,14 +37,68 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun AppScreen() {
+    MainProfile("Jennifer Doe", "Android Developer Extraordinaire")
 }
 
-@Preview(showBackground = true)
+@Composable
+private fun MainProfile(name: String, position: String) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        val image = painterResource(id = R.drawable.android_logo)
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .width(80.dp)
+                .height(80.dp)
+        )
+        Text(
+            text = name,
+            color = Color.White,
+            fontSize = 48.sp,
+            fontWeight = FontWeight.Light,
+            modifier = Modifier.padding(bottom =4.dp)
+
+        )
+        Text(
+            text = position,
+            color = Color(0xFF3ddc84)
+        )
+    }
+}
+
+@Composable
+fun AdditionalInfoRow() {
+    Row() {
+        Icon(
+            imageVector = Icons.Filled.Phone,
+            contentDescription = "Phone number",
+            tint = Color(0xFF3ddc84)
+        )
+    }
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFF0a3041
+)
 @Composable
 fun DefaultPreview() {
     BusinessCardTheme {
-        Greeting("Android")
+        AppScreen()
+    }
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFF0a3041
+)
+@Composable
+fun AdditionalInfoRowPreview() {
+    BusinessCardTheme {
+        AdditionalInfoRow()
     }
 }
