dd10519c80dba5c88520db7e20328234e1ad0bc3
Jaehyun <recreate@spass.ai>
2022-12-28 11:13:01 +0900

    Done "BusinessCard" app

--- BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
+++ BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -7,12 +7,16 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,14 +42,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AppScreen() {
-    MainProfile("Jennifer Doe", "Android Developer Extraordinaire")
-}
-
-@Composable
-private fun MainProfile(name: String, position: String) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
+    ) {
+        MainProfile(
+            name = "Jennifer Doe",
+            position = "Android Developer Extraordinaire",
+            modifier = Modifier.weight(1f)
+        )
+        Column(modifier = Modifier.padding(bottom = 48.dp)) {
+            AdditionalInfoRow(Icons.Filled.Phone, stringResource(R.string.phone_number), "+11 (123) 444 555 666")
+            AdditionalInfoRow(Icons.Filled.Share, stringResource(R.string.share), "@AndroidDev")
+            AdditionalInfoRow(Icons.Filled.Email, stringResource(R.string.email), "jen.doe@android.com")
+        }
+    }
+}
+
+@Composable
+private fun MainProfile(name: String, position: String, modifier: Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxWidth()
     ) {
         val image = painterResource(id = R.drawable.android_logo)
         Image(
@@ -71,13 +90,25 @@ private fun MainProfile(name: String, position: String) {
 }
 
 @Composable
-fun AdditionalInfoRow() {
-    Row() {
-        Icon(
-            imageVector = Icons.Filled.Phone,
-            contentDescription = "Phone number",
-            tint = Color(0xFF3ddc84)
+fun AdditionalInfoRow(imageVector: ImageVector, infoDescription: String, value: String) {
+    Column {
+        Divider(
+            modifier = Modifier.fillMaxWidth(),
+            thickness = 1.8.dp,
+            color = Color(0xFF406272)
         )
+        Row(modifier = Modifier.padding(horizontal = 48.dp, vertical = 8.dp)) {
+            Icon(
+                imageVector = imageVector,
+                contentDescription = infoDescription,
+                tint = Color(0xFF3ddc84)
+            )
+            Text(
+                text = value,
+                color = Color.White,
+                modifier = Modifier.padding(start = 16.dp)
+            )
+        }
     }
 }
 
@@ -99,6 +130,6 @@ fun DefaultPreview() {
 @Composable
 fun AdditionalInfoRowPreview() {
     BusinessCardTheme {
-        AdditionalInfoRow()
+        AdditionalInfoRow(Icons.Filled.Email, "Email", "jen.doe@android.com")
     }
 }
